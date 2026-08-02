0beee764e1fb909a4dae8c52fa8eaca3994d335f
tienze08 <tienvh08082003@gmail.com>
2026-08-02 15:30:38 +0700

    build deploy backend

--- vocapet-desktop-friend/src/api/client.ts
+++ vocapet-desktop-friend/src/api/client.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+const API_URL = (import.meta.env.VITE_API_URL ?? "http://localhost:8080").replace(/\/$/, "");
 
 export async function apiFetch<T>(
   endpoint: string,
